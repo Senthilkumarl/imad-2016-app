0a529f8c0126ac24e148fd110a69e3bc4ec07d50
Senthilkumarl <senthilkumarl@live.com>
2016-09-23 19:39:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+//Chamge the text
+var element=document.getElementById('main-text');
+element.innerHTML='New value'
+//move the image
+var img=document.getElementById('madi');
+//console.log(img);
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft + 1;
+    img.style.marginLeft=marginLeft + 'px';
+};
+
+img.onclick=function () {
+    var interval=setInterval(moveRight,50)    
+};
